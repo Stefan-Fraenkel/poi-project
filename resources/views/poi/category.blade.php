c5a008e20e3a60e3de0f458377a11222109d2d5d
Stefan <stefan.fraenkel@gmail.com>
2022-01-18 17:38:40 +0100

    Update category.blade.php
    add inputs for experimenting

--- resources/views/poi/category.blade.php
+++ resources/views/poi/category.blade.php
@@ -12,6 +12,47 @@
             <input type="checkbox" name="categories[]" value="{{$category}}"> {{$category}} &nbsp;&nbsp;&nbsp;&nbsp;
         @endforeach
         <br><br>
+
+        <div class="container pt-4">
+            <div class="row">
+                <div class="col-6">
+                    <div class="input-group">
+                        <select class="custom-select" name="rating" id="rating_select">
+                            <option selected>Choose...</option>
+                            <option value="1">1 Stern</option>
+                            <option value="2">2 Sterne</option>
+                            <option value="3">3 Sterne</option>
+                            <option value="4">4 Sterne</option>
+                            <option value="5">5 Sterne</option>
+                        </select>
+                        <div class="input-group-append">
+                            <button class="edit btn btn-dark text-uppercase" type="button">Filtern</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="container pt-4">
+            <div class="row">
+                <div class="col-6">
+                    <div class="input-group">
+                        <select class="custom-select" name="distance" id="distance_select">
+                            <option selected>Choose...</option>
+                            <option value="1">1 km</option>
+                            <option value="5">5 km</option>
+                            <option value="10">10 km</option>
+                            <option value="20">20 km</option>
+                            <option value="25">25 km</option>
+                        </select>
+                        <div class="input-group-append">
+                            <button class="edit btn btn-dark text-uppercase" type="button">Filtern</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br>
         <div class="d-flex align-items-baseline" >
             <div class="control">
                 <button class="edit btn btn-dark text-uppercase" type="submit" >Suchen</button>
