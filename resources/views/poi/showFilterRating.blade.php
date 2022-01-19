9c982b9a58f22952873ab7ca857b6356f51ae755
Stefan <stefan.fraenkel@gmail.com>
2022-01-19 10:01:29 +0100

    add service to determine user location

--- resources/views/poi/showFilterRating.blade.php
+++ resources/views/poi/showFilterRating.blade.php
@@ -8,7 +8,7 @@
 
     <div class="container">
         @foreach($theresa as $poi)
-            <p> Bewertungen: {{$poi[0]->durchschnittsbewertung}}</p>
+            <p> Bewertungen: {{$poi[0]->rating}}</p>
         @endforeach
     </div>
 
@@ -23,11 +23,6 @@
                     <option value="3">3 Sterne</option>
                     <option value="4">4 Sterne</option>
                     <option value="5">5 Sterne</option>
-                    <option value="6">6 Sterne</option>
-                    <option value="7">7 Sterne</option>
-                    <option value="8">8 Sterne</option>
-                    <option value="9">9 Sterne</option>
-                    <option value="10">10 Sterne</option>
                 </select>
                 <div class="input-group-append">
                     <button class="edit btn btn-dark text-uppercase" type="button">Filtern</button>
