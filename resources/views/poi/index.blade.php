d8e05a7061e5ada7ca5d2072975980f86876cde5
theresa-dev <ts@eberl-online.de>
2022-02-02 10:39:41 +0100

    first design changes poi cards

--- resources/views/poi/index.blade.php
+++ resources/views/poi/index.blade.php
@@ -1,21 +1,21 @@
 @extends('adminlte::page')
-
 @section('content_header')
-    <h2 class="h4 font-weight-bold">Übersicht POIs: {{$category}}</h2>
+    <h2 class="h4 font-weight-bold" id="demo">Übersicht POIs: {{$category}}</h2>
 @stop
 
 @section('content')
     <div class="container">
         <div class="row align-items-start">
-            <div class="col">
-                @foreach($pois as $poi)
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{$poi->photo}}"
+            @foreach($pois as $poi)
+
+                <div class="col-sm-12 col-md-6 col-xl-4">
+                    <div class="card">
+                        <img src="{{$poi->photo}}" style="max-height: 192px;"
                              class="card-img-top" alt="fallback bild">
                         <div class="card-body">
                             <h5 class="card-title">{{$poi->poi_name}}</h5>
                             <p class="card-text">{{$poi->description}}
-                            <br><br>
+                                <br><br>
                                 Entfernung: {{$poi->distance}} km
                             </p>
                             <p class="poi-rating-text"><i class="rating-icon fas fa-star"></i>{{$poi->rating}}
@@ -25,9 +25,69 @@
                             </div>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
 
+
+            @endforeach
         </div>
     </div>
+
+
+    @push('css')
+
+        <style>
+
+
+            .card {
+                height: 500px;
+                width: 18rem;
+                margin-top: 25px;
+            }
+
+            .card-footer {
+                background-color: white;
+                padding-left: 0;
+                margin-top: 18px;
+            }
+
+            .ratings {
+                margin-top: 30px;
+                text-align: center;
+                padding: 10px 10px 0 10px;
+
+            }
+
+            .rating-icon {
+                color: gold;
+                padding-right: 5px;
+            }
+
+            .poi-more-btn {
+                margin-top: 5px;
+            }
+
+            .poi-rating-text {
+                font-weight: bold;
+                margin-bottom: 0;
+                font-size: 18px;
+            }
+
+            @media (max-width: 576px){
+                .card{
+                    height: 1000px;
+                    width: 10px;
+                }
+            }
+            @media (max-width: 992px){
+                .card {
+                    height: 500px;
+                    width: 100%;
+                    margin-top: 25px;
+                }
+            }
+
+        </style>
+
+    @endpush
 @stop
+
