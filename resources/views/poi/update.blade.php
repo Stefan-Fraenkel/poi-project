7fb793606789a35820a18d4d7fdee9e6ba6544f4
Stefan <stefan.fraenkel@gmail.com>
2022-02-21 09:50:38 +0100

    Update update.blade.php
    improve code

--- resources/views/poi/update.blade.php
+++ resources/views/poi/update.blade.php
@@ -9,33 +9,33 @@
     <form method="POST" action="/poi/update">
         @csrf
         <label for="poiName" class="form-label">POI Name</label>
-        <input name="poi_name" type="text" id="poiName" class="form-control" required value="{{$poi[0]->poi_name}}">
+        <input name="poi_name" type="text" id="poiName" class="form-control" required value="{{$poi->poi_name}}">
         <label for="poiStrasse" class="from-label">Strasse</label>
-        <input name="street" type="text" id="poiStrasse" class="form-control"required value="{{$poi[0]->street}}">
+        <input name="street" type="text" id="poiStrasse" class="form-control"required value="{{$poi->street}}">
         <label for="poiPlz" class="form-label">PLZ</label>
-        <input name="zipcode" type="number" id="poiPlz" class="form-control"required value="{{$poi[0]->zipcode}}">
+        <input name="zipcode" type="number" id="poiPlz" class="form-control"required value="{{$poi->zipcode}}">
         <label for="poiOrt" class="from-label">Ort</label>
-        <input name="city" type="text" id="poiOrt" class="form-control"required value="{{$poi[0]->city}}">
+        <input name="city" type="text" id="poiOrt" class="form-control"required value="{{$poi->city}}">
 
         <label for="poiBeschreibung" class="form-label">Beschreibung</label>
-        <textarea name="description" id="poiBeschreibung" class="form-control" value="{{$poi[0]->description}}"></textarea>
+        <textarea name="description" id="poiBeschreibung" class="form-control" value="{{$poi->description}}"></textarea>
 
         <label for="openingHours" class="from-label">Öffnungszeiten</label>
-        <input name="open" type="text" id="openingHours" class="form-control" value="{{$poi[0]->open}}">
+        <input name="open" type="text" id="openingHours" class="form-control" value="{{$poi->open}}">
 
         <label for="website" class="from-label">Website</label>
-        <input name="website" type="text" id="website" class="form-control" value="{{$poi[0]->website}}">
+        <input name="website" type="text" id="website" class="form-control" value="{{$poi->website}}">
 
         <label for="photo" class="from-label">Foto</label>
-        <input name="photo" type="text" id="photo" class="form-control" value="{{$poi[0]->photo}}">
+        <input name="photo" type="text" id="photo" class="form-control" value="{{$poi->photo}}">
 
         <label for="poiLong" class="form-label">Längengrad</label>
-        <input name="long" type="number" id="poiLong" class="form-control"value="{{$poi[0]->long}}">
+        <input name="long" type="number" id="poiLong" class="form-control"value="{{$poi->long}}">
 
         <label for="poiLat" class="form-label">Breitengrad</label>
-        <input name="lat" type="number" id="poiLat" class="form-control" value="{{$poi[0]->lat}}">
+        <input name="lat" type="number" id="poiLat" class="form-control" value="{{$poi->lat}}">
 
-        <input type="hidden" name="poi_id" value="{{$poi[0]->poi_id}}">
+        <input type="hidden" name="poi_id" value="{{$poi->poi_id}}">
 
         <div class="d-flex align-items-baseline" >
             <div class="control">
