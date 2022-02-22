a1c27cf105a66408ebb1604b27f2fa2ff1ae6c14
Stefan <stefan.fraenkel@gmail.com>
2022-02-22 12:29:12 +0100

    Update POIController.php
    removed unnecassary import

--- app/Http/Controllers/POIController.php
+++ app/Http/Controllers/POIController.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Stevebauman\Location\Facades\Location;
-use function PHPUnit\Framework\returnValueMap;
 
 
 class POIController extends BaseController
